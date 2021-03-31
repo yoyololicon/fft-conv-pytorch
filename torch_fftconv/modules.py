3aeae6734d615783d853ea6ab7f6b4e7efb1a8ad
Chin Yun Yu <ya70201@gmail.com>
2021-03-31 22:05:34 +0800

    fix: call fft_conv3d in forward call

--- torch_fftconv/modules.py
+++ torch_fftconv/modules.py
@@ -32,10 +32,10 @@ class FFTConv2d(nn.Conv2d):
 
 
 class FFTConv3d(nn.Conv3d):
-    def _conv_forward(self, input: Tensor, weight: Tensor, bias: Optional[Tensor]):
+    def forward(self, input: Tensor) -> Tensor:
         if self.padding_mode != 'zeros':
             return fft_conv3d(F.pad(input, self._reversed_padding_repeated_twice, mode=self.padding_mode),
-                              weight, bias, self.stride,
-                              _triple(0), self.dilation, self.groups)
-        return fft_conv3d(input, weight, bias, self.stride,
+                              self.weight, self.bias, self.stride, _triple(0),
+                              self.dilation, self.groups)
+        return fft_conv3d(input, self.weight, self.bias, self.stride,
                           self.padding, self.dilation, self.groups)
