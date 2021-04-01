353fbaf4c015b315057f17b3a4de39a7ea674da0
Joey Yu <joeyyu@rayark.com>
2021-04-01 11:01:16 +0800

    refactor: unified modules to overwrite forward call directly

--- torch_fftconv/modules.py
+++ torch_fftconv/modules.py
@@ -2,7 +2,6 @@ import torch.nn as nn
 from torch import Tensor
 import torch.nn.functional as F
 from torch.nn.modules.utils import _single, _pair, _triple
-from typing import Optional
 from .functional import *
 
 
@@ -12,22 +11,22 @@ __all__ = [
 
 
 class FFTConv1d(nn.Conv1d):
-    def _conv_forward(self, input: Tensor, weight: Tensor, bias: Optional[Tensor]):
+    def forward(self, input: Tensor) -> Tensor:
         if self.padding_mode != 'zeros':
             return fft_conv1d(F.pad(input, self._reversed_padding_repeated_twice, mode=self.padding_mode),
-                              weight, bias, self.stride,
-                              _single(0), self.dilation, self.groups)
-        return fft_conv1d(input, weight, bias, self.stride,
+                              self.weight, self.bias, self.stride, _single(0),
+                              self.dilation, self.groups)
+        return fft_conv1d(input, self.weight, self.bias, self.stride,
                           self.padding, self.dilation, self.groups)
 
 
 class FFTConv2d(nn.Conv2d):
-    def _conv_forward(self, input: Tensor, weight: Tensor, bias: Optional[Tensor]):
+    def forward(self, input: Tensor) -> Tensor:
         if self.padding_mode != 'zeros':
             return fft_conv2d(F.pad(input, self._reversed_padding_repeated_twice, mode=self.padding_mode),
-                              weight, bias, self.stride,
-                              _pair(0), self.dilation, self.groups)
-        return fft_conv2d(input, weight, bias, self.stride,
+                              self.weight, self.bias, self.stride, _pair(0),
+                              self.dilation, self.groups)
+        return fft_conv2d(input, self.weight, self.bias, self.stride,
                           self.padding, self.dilation, self.groups)
 
 
