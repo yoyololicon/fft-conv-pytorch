29e4a6f63f8773e1b29a4f67f5357817cc27e54e
Chin Yun Yu <ya70201@gmail.com>
2021-04-02 11:06:07 +0800

    draft

--- torch_fftconv/modules.py
+++ torch_fftconv/modules.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 from torch import Tensor
 import torch.nn.functional as F
 from torch.nn.modules.utils import _single, _pair, _triple
-from typing import Optional
+from typing import Optional, List
 from .functional import *
 
 
@@ -39,3 +39,51 @@ class FFTConv3d(nn.Conv3d):
                               self.dilation, self.groups)
         return fft_conv3d(input, self.weight, self.bias, self.stride,
                           self.padding, self.dilation, self.groups)
+
+
+class FFTConvTranspose1d(nn.ConvTranspose1d):
+    def forward(self, input: Tensor, output_size: Optional[List[int]] = None) -> Tensor:
+        if self.padding_mode != 'zeros':
+            raise ValueError(
+                'Only `zeros` padding mode is supported for FFTConvTranspose1d')
+
+        # One cannot replace List by Tuple or Sequence in "_output_padding" because
+        # TorchScript does not support `Sequence[T]` or `Tuple[T, ...]`.
+        output_padding = self._output_padding(
+            input, output_size, self.stride, self.padding, self.kernel_size, self.dilation)  # type: ignore
+        return fft_conv_transpose1d(
+            input, self.weight, self.bias, self.stride, self.padding,
+            output_padding, self.groups, self.dilation)
+
+
+
+
+class FFTConvTranspose2d(nn.ConvTranspose2d):
+    def forward(self, input: Tensor, output_size: Optional[List[int]] = None) -> Tensor:
+        if self.padding_mode != 'zeros':
+            raise ValueError(
+                'Only `zeros` padding mode is supported for FFTConvTranspose2d')
+
+        # One cannot replace List by Tuple or Sequence in "_output_padding" because
+        # TorchScript does not support `Sequence[T]` or `Tuple[T, ...]`.
+        output_padding = self._output_padding(
+            input, output_size, self.stride, self.padding, self.kernel_size, self.dilation)  # type: ignore
+        return fft_conv_transpose2d(
+            input, self.weight, self.bias, self.stride, self.padding,
+            output_padding, self.groups, self.dilation)
+
+
+
+class FFTConvTranspose3d(nn.ConvTranspose3d):
+    def forward(self, input: Tensor, output_size: Optional[List[int]] = None) -> Tensor:
+        if self.padding_mode != 'zeros':
+            raise ValueError(
+                'Only `zeros` padding mode is supported for FFTConvTranspose3d')
+
+        # One cannot replace List by Tuple or Sequence in "_output_padding" because
+        # TorchScript does not support `Sequence[T]` or `Tuple[T, ...]`.
+        output_padding = self._output_padding(
+            input, output_size, self.stride, self.padding, self.kernel_size, self.dilation)  # type: ignore
+        return fft_conv_transpose3d(
+            input, self.weight, self.bias, self.stride, self.padding,
+            output_padding, self.groups, self.dilation)
